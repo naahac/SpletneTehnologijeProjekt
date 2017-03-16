a77a573b871e4e3b820ebc4fde8f7b21bda7b90c
cimperski <samo.taciga@gmail.com>
2017-03-16 19:10:54 +0100

    token

--- models/token.js
+++ models/token.js
@@ -1,11 +1,42 @@
+var tokens = []
+
 class Token { 
-  constructor(tokenID, token, createdate, active, userID) {
+  constructor(tokenID, createDate, active, userID) {
     this.tokenID = tokenID;
-    this.token = token;
-    this.createdate = createdate;
+    this.createDate = createDate;
     this.active = active;
     this.userID = userID;
   }
+
+  static createToken(userID){
+    var tokenID = CreateGUID();
+    tokens.push(tokenID, Date.now(), true, userID);
+    return tokenID;
+  }
+
+  static login(username, password){
+
+  }
+
+  static logoff(tokenID){
+
+  }
+
+  static changeStatus(TokenID){
+    var token = tokens.find(o => o.tokenID === tokenID);
+    var index = tokens.indexOf(token);
+    tokens[index].active = false;
+  }
+
+  S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+  }
+
+  CreateGUID(){
+    return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+  }
 }
+
+tokens.push(createToken(1));
 
 module.exports = Token;
