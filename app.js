943d1429134b11d0fec371e24b77da5684b21100
Natanael Ahac <natanael.ahac@gmail.com>
2017-03-24 15:50:43 +0100

    added www

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var users = require('./routes/users');
 // var pictures = require('./routes/pictures');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -64,7 +63,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
